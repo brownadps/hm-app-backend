2087985289c8996fa8045663da2d9dde84a4a1c0
Jenna Tishler <jenna_tishler@brown.edu>
2019-03-23 15:08:04 -0400

    database rename stuff

--- db.js
+++ db.js
@@ -167,7 +167,7 @@ const groupActions = {
     getGroupIds: async function(db) {
         let results;
         try {
-            results = await db.query("SELECT id FROM pairs");
+            results = await db.query("SELECT id FROM shiftgroups");
         } catch (err) {
             console.error(err);
             throw new Error(JSON.stringify({
@@ -191,7 +191,7 @@ const groupActions = {
     getGroupById: async function(db, groupId) {
         let results;
         try {
-            results = await db.query("SELECT id, user_id1, user_id2, user_id3 FROM pairs WHERE id = ?", [groupId]);
+            results = await db.query("SELECT id, user_id1, user_id2, user_id3 FROM shiftgroups WHERE id = ?", [groupId]);
         } catch (err) {
             console.error(err);
             throw new Error(JSON.stringify({
@@ -215,7 +215,7 @@ const groupActions = {
     getGroupByUserId: async function(db, userId) {
         let results;
         try {
-            results = await db.query("SELECT p.id as pairs_id, p.user_id1, p.user_id2, p.user_id3 FROM users INNER JOIN pairs AS p ON users.id = p.user_id1 OR users.id = p.user_id2 OR users.id = p.user_id3 WHERE users.id = ?", [userId]);
+            results = await db.query("SELECT g.id as group_id, g.user_id1, g.user_id2, g.user_id3 FROM users INNER JOIN shiftgroups AS g ON users.id = g.user_id1 OR users.id = g.user_id2 OR users.id = g.user_id3 WHERE users.id = ?", [userId]);
         } catch (err) {
             console.error(err);
             throw new Error(JSON.stringify({
@@ -230,7 +230,7 @@ const groupActions = {
             throw new Error(JSON.stringify({
                 status: 404,
                 data: {
-                    message: "User ID not found or is not in a pair."
+                    message: "User ID not found or is not in a group."
                 }
             }));
         }
@@ -240,15 +240,15 @@ const groupActions = {
 
 const otherActions = {
     getGroupMembers: function(conn, groupId) {
-        conn.query("SELECT pairs.id, pairs.user_id1, pairs.user_id2, pairs.user_id3, users.first_name, users.last_name FROM pairs, users WHERE pairs.id = ? AND (users.id = pairs.user_id1 OR users.id = pairs.user_id2 OR users.id = pairs.user_id3)", [groupId], 
+        conn.query("SELECT shiftgroups.id, shiftgroups.user_id1, shiftgroups.user_id2, shiftgroups.user_id3, users.first_name, users.last_name FROM shiftgroups, users WHERE shiftgroups.id = ? AND (users.id = shiftgroups.user_id1 OR users.id = shiftgroups.user_id2 OR users.id = shiftgroups.user_id3)", [groupId], 
             function(error, result, fields) {
                 console.log(result);
         });
     },
     getCCShifts: function(conn, ccId) {
         conn.query("SELECT cleaningshifts.room, cleaningshifts.size, cleaningshifts.day, " + 
-            "cleaningshifts.pair_id, p.user_id1, p.user_id2, p.user_id3 FROM cleaningshifts " + 
-            "INNER JOIN pairs AS p ON cleaningshifts.pair_id = p.id WHERE cleaningshifts.cc_id = ?", 
+            "cleaningshifts.group_id, g.user_id1, g.user_id2, g.user_id3 FROM cleaningshifts " + 
+            "INNER JOIN shiftgroups AS g ON cleaningshifts.group_id = g.id WHERE cleaningshifts.cc_id = ?", 
             [ccId], function(error, result, fields) {
                 console.log(result);
                 //console.error(error);
@@ -264,12 +264,12 @@ const otherActions = {
                 "GROUP_CONCAT(DISTINCT u2.first_name SEPARATOR ', ') as cc_names, " +
                 "GROUP_CONCAT(DISTINCT u2.email SEPARATOR ', ') as cc_emails " +
                 "FROM cleaningshifts " + 
-                "INNER JOIN pairs as p on cleaningshifts.pair_id = p.id " +
-                "RIGHT JOIN users as u ON p.user_id1 = u.id OR p.user_id2 = u.id OR p.user_id3 = u.id " +
+                "INNER JOIN shiftgroups as g on cleaningshifts.group_id = g.id " +
+                "RIGHT JOIN users as u ON g.user_id1 = u.id OR g.user_id2 = u.id OR g.user_id3 = u.id " +
                 "LEFT JOIN ccshifts AS cc ON ISNULL(cc.id) OR cleaningshifts.cc_id = cc.id " +
-                "LEFT JOIN pairs AS p2 on cc.id IS NOT NULL AND cc.pair_id = p2.id " +
-                "LEFT JOIN users as u2 ON cc.id IS NOT NULL AND (p2.user_id1 = u2.id OR p2.user_id2 = u2.id OR p2.user_id3 = u2.id) " +
-                "WHERE day = ? GROUP BY cleaningshifts.id, cc.id, p.id, p2.id", [date]);
+                "LEFT JOIN shiftgroups AS g2 on cc.id IS NOT NULL AND cc.group_id = g2.id " +
+                "LEFT JOIN users as u2 ON cc.id IS NOT NULL AND (g2.user_id1 = u2.id OR g2.user_id2 = u2.id OR g2.user_id3 = u2.id) " +
+                "WHERE day = ? GROUP BY cleaningshifts.id, cc.id, g.id, g2.id", [date]);
         } catch (err) {
             console.error(err);
             throw new Error(JSON.stringify({
@@ -323,9 +323,9 @@ const otherActions = {
         try {
             results = await db.query("SELECT room, size, day, " +
                 "null as start_day, null as end_day FROM cleaningshifts " +
-                "WHERE pair_id = ? AND day >= ? " +
+                "WHERE group_id = ? AND day >= ? " +
                 "UNION SELECT room, null as size, null as day, start_day, end_day " +
-                "FROM ccshifts WHERE pair_id = ? AND start_day >= ?", [groupId, date, groupId, date]);
+                "FROM ccshifts WHERE group_id = ? AND start_day >= ?", [groupId, date, groupId, date]);
         } catch (err) {
             console.error(err);
             throw new Error(JSON.stringify({
@@ -348,9 +348,9 @@ const otherActions = {
     },
     createShift: async function(db, shiftInfo) {
         try {
-            const results = await db.query("INSERT INTO cleaningshifts (room, size, points, pair_id, day, status) " +
+            const results = await db.query("INSERT INTO cleaningshifts (room, size, points, group_id, day, status) " +
             "VALUES (?, ?, ?, ?, ?, 'ASSIGNED')", [shiftInfo.room, shiftInfo.size, 
-            shiftInfo.points, shiftInfo.pair_id, shiftInfo.day]);
+            shiftInfo.points, shiftInfo.group_id, shiftInfo.day]);
             const update_results = await db.query("UPDATE cleaningshifts " +
                 "RIGHT JOIN ccshifts ON ccshifts.room = cleaningshifts.room AND ccshifts.start_day <= cleaningshifts.day AND ccshifts.end_day >= cleaningshifts.day " +
                 "SET cleaningshifts.cc_id = ccshifts.id " +
@@ -375,8 +375,8 @@ const otherActions = {
         }
     },
     addCCShift: function(conn, ccShiftInfo) {
-        conn.query("INSERT INTO ccshifts (room, pair_id, start_day, end_day, missed, made_up) " +
-            "VALUES (?, ?, ?, ?, 0, 0)", [ccShiftInfo.room, ccShiftInfo.pair_id, 
+        conn.query("INSERT INTO ccshifts (room, group_id, start_day, end_day, missed, made_up) " +
+            "VALUES (?, ?, ?, ?, 0, 0)", [ccShiftInfo.room, ccShiftInfo.group_id, 
             ccShiftInfo.start_day, ccShiftInfo.end_day], function(error, result, fields) {
                 conn.query("UPDATE cleaningshifts SET cc_id = ? WHERE room = ? AND day >= ? AND " +
                     "day <= ?", [ccShiftInfo.cc_id, ccShiftInfo.room, ccShiftInfo.start_day, 
@@ -387,8 +387,8 @@ const otherActions = {
     },
     createCCShift: async function(db, ccShiftInfo) {
         try {
-            const results = await db.query("INSERT INTO ccshifts (room, pair_id, start_day, end_day, missed, made_up) " +
-                "VALUES (?, ?, ?, ?, 0, 0)", [ccShiftInfo.room, ccShiftInfo.pair_id, 
+            const results = await db.query("INSERT INTO ccshifts (room, group_id, start_day, end_day, missed, made_up) " +
+                "VALUES (?, ?, ?, ?, 0, 0)", [ccShiftInfo.room, ccShiftInfo.group_id, 
                 ccShiftInfo.start_day, ccShiftInfo.end_day]);
             const update_results = await db.query("UPDATE cleaningshifts SET cc_id = ? WHERE room = ? AND day >= ? AND " +
                     "day <= ?", [results.id, ccShiftInfo.room, ccShiftInfo.start_day, 
@@ -436,7 +436,7 @@ const otherActions = {
         try {
             let user_id1, user_id2, user_id3;
             [user_id1, user_id2, user_id3] = userIds;
-            await db.query('INSERT INTO pairs (user_id1, user_id2, user_id3) VALUES (?, ?, ?)', [user_id1, user_id2, user_id3]);
+            await db.query('INSERT INTO shiftgroups (user_id1, user_id2, user_id3) VALUES (?, ?, ?)', [user_id1, user_id2, user_id3]);
             return {
                 status: 200,
                 data: {
